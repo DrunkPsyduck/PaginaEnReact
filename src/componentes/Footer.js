5d774f7385f59ff4ab6c5174d277570698fa0323
DrunkPsyduck <40211718+DrunkPsyduck@users.noreply.github.com>
2020-02-18 12:13:16 +0100

    Update Footer.js

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -9,7 +9,8 @@ class Footer extends React.Component {
         return ( 
             <React.Fragment>
                 <footer style={footer}>
-                    <div><p>Hola mundo </p> </div>
+                    < div > < p > Lorem ipsum dolor sit amet, consectetur adipiscing elit.Proin fermentum, urna vel rutrum ullamcorper, urna felis cursus sapien, id ornare ligula urna sed eros.In ornare eu justo vitae pharetra.Nunc maximus dolor augue.Nulla ac vulputate ipsum.Cras et metus sit amet elit vulputate condimentum et a massa.Vivamus a rutrum leo, id fringilla lorem.
+                     </p> </div >
                 </footer>
             </React.Fragment>
         );

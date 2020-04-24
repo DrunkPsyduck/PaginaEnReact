37cdb479521a89bf6866b410a0546fb4668e7db3
DrunkPsyduck <40211718+DrunkPsyduck@users.noreply.github.com>
2020-04-24 11:46:44 +0200

    Cambios documento

--- src/componentes/Cuerpo.js
+++ src/componentes/Cuerpo.js
@@ -24,33 +24,7 @@ class Cuerpo extends React.Component {
               Si buscas la historio de un chico que aprendio a programar de
               manera autodidacta con 10 años y que crecio entre ordenadores y
               creando programas, entonces abandona esta página, por que esa no
-              es mi historia. Si de verdad quieres conocerla, sigue leyendo.{" "}
-              <br />
-              Prodría contarte mi vida desde que nací pero prefiero ahorrarte el
-              tostón. Por ello contare mi andanza por la ESO y cursos
-              posteriores. En la ESO nunca fui un estudiante modelo, por no
-              serlo no fui ni estudiante promedio. Más bien era el estudiante
-              vago que solo aprobaba las asignaturas más sencillas. No estudiaba
-              y me pasaba las tardes viendo la televisión o jugando en un equipo
-              de baloncesto. repeti varios cursos. Para mi el instituto era como
-              un castigo, no me sentia cómodo y no sentía que contase. De hecho,
-              en esa parte de mi vida, era bastante tímido y el constante
-              rechazo por parte de ciertos profesores me hizo aún más
-              introvertido. La ESO fue pasando sin pena ni gloria. En 4º decidí
-              continuar estudiando un grado medio, del que tampoc estudiaba. Así
-              me fue...
-              <br />
-              Algunos dirían que no aprendí, incluso me llegarón a decir que
-              esto no era lo mío y que buscase otra cosa... Creo que se
-              equivocaba y si algún día lee esto, que sepa que si lo conseguí y
-              no gracias a el.
-              <br />
-              Esta es parte de mi historia, pero no me arrepiento de ello,
-              gracias a ello aprendí bastantes cosas, tarde pero las aprendí.
-              Gracias a esas lecciones pasadas hoy soy mejor. Gracias a esas
-              lecciones he aprendido diversas cosas incluyendo mejoras a nivel
-              personal y educativo/profesional. Entre ellas:
-              <hr />
+              es mi historia...
               <ul style={{ listStyleType: "none"}}>
                 <li>HTML5 + CSS3 + JS ES6</li>
                 <li>MySQL</li>
